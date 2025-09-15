2208de7a787514845e2e42c9fe7187e713190a6a
Rashidtv698 <134952935+Rashidtv698@users.noreply.github.com>
2025-09-15 15:50:40 +0530

    Add files via upload

---
+++ passmark.py
@@ -0,0 +1,9 @@
+
+for i in range(6):
+    
+    num =int(input("enter the mark to check passed"))
+    if num>=40:
+        print("passed with "+ str(num)+"mark")
+    else:
+        print("failed")
+
